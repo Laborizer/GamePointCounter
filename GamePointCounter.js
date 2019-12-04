79df4663b3c1d789b54604d49bb64e0973194d6b
Lauri <laiho.lauri@luukku.com>
2019-12-04 16:55:50 +0200

    Add basic layout

--- GamePointCounter.js
+++ GamePointCounter.js
@@ -1,14 +1,20 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity} from 'react-native';
 
 export default class GamePointCounter extends Component {
   state = {
+    team1Name: "Team1",
+    team2Name: "Team2",
     team1Points: 0,
     team2Points: 0,
   }
   render() {
     return (
       <View style={styles.container}>
+        <View style={styles.teamLabelContainer}>
+          <Text style={{flex: 1, alignSelf: 'center', textAlign: "center"}}>{this.state.team1Name}</Text>
+          <Text style={{flex: 1, alignSelf: 'center', textAlign: "center"}}>{this.state.team2Name}</Text>
+        </View>
         <View style={styles.teamsContainer}>
           <View style={styles.team1Container}>
             <Text>{this.state.team1Points}</Text>
@@ -17,8 +23,16 @@ export default class GamePointCounter extends Component {
             <Text>{this.state.team2Points}</Text>
           </View>
         </View>
-        <View>
-          
+        <View style={styles.buttonsContainer}>
+          <View style={styles.buttonsContainer}>
+            <Button title="+"></Button>
+            <Button title="-"></Button>
+          </View>
+          <Button title="Reset"></Button>
+          <View style={styles.buttonsContainer}>
+            <Button title="+"></Button>
+            <Button title="-"></Button>
+          </View>
         </View>
       </View>
     );
@@ -26,11 +40,19 @@ export default class GamePointCounter extends Component {
 }
 
 const styles = StyleSheet.create({
-  teamsContainer: {
+  teamLabelContainer: {
     flex: 1,
     flexDirection: 'row',
-    alignItems: 'center',
+    backgroundColor: '#d1e2d3',
+    alignItems: 'stretch',
     justifyContent: 'center'
+  },
+  teamsContainer: {
+    flex: 2,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#d1ae94',
   },
   team1Container: {
     flex: 1,
@@ -54,8 +76,15 @@ const styles = StyleSheet.create({
     borderColor: '#ea4d4d',
     borderWidth: 4,
   },
-  container: {
+  buttonsContainer: {
     flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#e4bc91',
+  },
+  container: {
+    flex: 3,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
